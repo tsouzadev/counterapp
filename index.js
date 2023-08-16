875dd48a8a80d288c2212b172de4dfae92cc6b31
Tiago <dev.tiagosouza@gmail.com>
2023-08-16 12:20:26 -0400

    Changed to implement prometheus client

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require("express");
 
-const app = express();
+const client = require("prom-client");
+const collectDefaultMetrics = client.collectDefaultMetrics;
+const Registry = client.Registry;
+const register = new Registry();
+collectDefaultMetrics({ register });
 
-let counter = 0;
-
-app.get("/", (req, res) => {
-  counter = counter + 1;
-  res.send(`Successful response. Accessed ${counter} times.`);
+const gauge = new client.Gauge({
+  name: "access_accumulator",
+  help: "app accumulator",
 });
 
-app.get("/metrics", (req, res) => {
-  res.send(`access_counter ${counter}`);
+register.registerMetric(gauge);
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  gauge.inc();
+  res.send(`Successful response. Accumulated ${await getAccumulatedValue()}.`);
+});
+
+app.get("/metrics", async (req, res) => {
+  res.send(await register.metrics());
+});
+
+app.get("/decrease", async (req, res) => {
+  if ((await getAccumulatedValue()) > 0) {
+    gauge.dec();
+  }
+
+  res.send(`Successful response. Accumulated ${await getAccumulatedValue()}.`);
 });
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
+
+async function getAccumulatedValue() {
+  return (await gauge.get()).values[0].value;
+}
